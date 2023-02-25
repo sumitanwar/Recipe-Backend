0bef09862065f20ec9d2b77395641fa52e3b1d92
Sumit Kumar <67133300+sumitanwar@users.noreply.github.com>
2023-02-25 17:22:52 +0530

    MDB URL Updated

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 function Server() {
-  const URI = "mongodb+srv://sumitku70:<password>@cluster0.0gkbz0l.mongodb.net/?retryWrites=true&w=majority";
+  const URI = "mongodb+srv://sumitku70:12345@cluster0.0gkbz0l.mongodb.net/recipes?retryWrites=true&w=majority";
   mongoose.set("strictQuery", false);
   //   mongoose.set("strictQuery", true);
   mongoose.connect(URI, (err) => {
