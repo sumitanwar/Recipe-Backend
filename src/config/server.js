dbd80d561d17cb0193a48b094483df6ac56d0ade
Sumitanwar <sumitku70@gmail.com>
2023-02-26 14:40:42 +0530

    server error fixed

--- src/config/server.js
+++ src/config/server.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 function Server() {
-<<<<<<< HEAD
   // const URI = process.env.URL_DB || "mongodb://127.0.0.1:27017/Recipe";
   const URI =
     "mongodb+srv://sumitku70:12345@cluster0.0gkbz0l.mongodb.net/recipes?retryWrites=true&w=majority";
-=======
-  const URI = "mongodb+srv://sumitku70:12345@cluster0.0gkbz0l.mongodb.net/recipes?retryWrites=true&w=majority";
->>>>>>> 0bef09862065f20ec9d2b77395641fa52e3b1d92
   mongoose.set("strictQuery", false);
   //   mongoose.set("strictQuery", true);
   mongoose.connect(URI, (err) => {
